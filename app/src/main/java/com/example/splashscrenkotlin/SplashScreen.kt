8e053bedc0dc7ce952dc4e6ea63a41c313ebff01
Md. Fazle Rabbi <fazle.rabbi@bracits.com>
2023-04-09 11:40:56 +0600

    add delay using looper

--- app/src/main/java/com/example/splashscrenkotlin/SplashScreen.kt
+++ app/src/main/java/com/example/splashscrenkotlin/SplashScreen.kt
@@ -4,15 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed({
+      /*  Handler().postDelayed({
             val iHome = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(iHome);
-        }, 4000)
+        }, 4000)*/
+
+        Looper.myLooper()?.let {
+            looper -> Handler(looper).postDelayed({
+            val iHome = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(iHome);
+        },4000)
+        }
 
     }
 }
